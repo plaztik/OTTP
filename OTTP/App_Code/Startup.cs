26fd0f07ae2957bb1a9720f2d2e73ced8f4edb64
tewy <tewy@CEAT-IT-PC005>
2019-01-14 12:09:53 -0600

    Add project files.

---
+++ OTTP/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OTTP.Startup))]
+namespace OTTP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
